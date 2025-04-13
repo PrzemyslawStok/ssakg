03ca5ba52e46d90fb27bf9d56db6690515c98095
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2025-04-13 21:46:30 +0200

    small corrections

--- ssakg/ssakg.py
+++ ssakg/ssakg.py
@@ -53,6 +53,8 @@ class SSAKG(ANAKG):
             return None
         sorted_elements, _ = self.order_sequence(unsorted_elements, ordering_alg=ordering_alg,
                                                  use_only_first_path=True)
+        if sorted_elements is None:
+            sorted_elements = unsorted_elements
 
         if decode_sequence:
             return self.decode_sequence(sorted_elements)
